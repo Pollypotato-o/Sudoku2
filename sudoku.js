53c1cb0870b0120585d0416f3a7d71751b1189b4
Igor <i.puzakov2010@mail.ru>
2022-11-26 16:30:40 +0300

    v1

--- sudoku.js
+++ sudoku.js
@@ -2,26 +2,47 @@
  * Принимает игровое поле в формате строки — как в файле sudoku-puzzles.txt.
  * Возвращает игровое поле после попытки его решить.
  * Договорись со своей командой, в каком формате возвращать этот результат.
- */ 
+ */
 function solve(boardString) {
+  const size = 9;
+  const boxSize = 3;
+  function solve(string) {
+    const arrBoard = stringToArr(string);
+    step = () => {
+      const currPos = findEmptySpace(arrBoard);
+      if (currPos === null) {
+        return true;
+      }
+      for (let num = 1; num <= size; num += 1) {
+        const isValid = validate(currPos, arrBoard, num);
+        if (isValid) {
+          const [y, x] = currPos;
+          arrBoard[y][x] = String(num);
+          if (step()) {
+            return true;
+          }
+          arrBoard[y][x] = "-";
+        }
+      }
+      return false;
+    };
+    step();
+    return arrBoard;
+  }
 }
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает булевое значение — решено это игровое поле или нет.
  */
-function isSolved(board) {
-
-}
+function isSolved(board) {}
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает строку с игровым полем для последующего вывода в консоль.
  * Подумай, как симпатичнее сформировать эту строку.
  */
-function prettyBoard(board) {
-
-}
+function prettyBoard(board) {}
 
 // Экспортировать функции для использования в другом файле (например, readAndSolve.js).
 module.exports = {
